f089b9aab6df66ba852c8a12b23bda3727c94312
ethanrichards <ethan.richards@indwes.edu>
2015-10-26 18:08:42 +0000

    Branched and completed?

--- Life.py
+++ Life.py
@@ -9,8 +9,8 @@ def main():
     world = []
     h = 80
     w = 22
-
-    populate(world,h,w)
+    userIn = ""
+    randomPopulate(world,h,w)
     display(world,h,w)
     
     userIn = input("Press a key, Q stops the program: ")
@@ -18,20 +18,69 @@ def main():
         
         display(world,h,w)
         
-        generation(world,h,w)
+        world = generation(world,h,w)
         
         userIn = input("Press a key, Q stops the program: ")
     print("Goodbye!")
-   #this function will populate the world 
+    
+def randomPopulate(world,h,w):
+    for x in range(h):
+        row = []
+        for y in range(w):
+            row.append(random.randint(0,1))
+        world.append(row)
+        
 def populate(world,h,w):
-   
-    pass
- #this function will display the world           
+    for x in range(h):
+        row = []
+        for y in range(w):
+            row.append(0)
+        world.append(row)  
+            
 def display(world,h,w):
-  
-    pass
-  #this function will create a new world for each generation and decide whether cells live or die  
+    worldString = "" 
+    for x in range(h):
+        rowString =""
+        for y in range(w):
+            if world[x][y] == 0:
+                rowString += " "
+            else:
+                rowString += "*"
+             
+        worldString += rowString + "\n"
+    print(worldString)
+        
+    
+    
 def generation(world,h,w):
-    pass
+    newWorld = []
+    populate(newWorld,h,w)
+        
+    for x in range(1,h-1):
+        for y in range(1,w-1):
+            n = 0
+            n = world[x-1][y-1] +  \
+                world[x-1][y] +  \
+                world[x-1][y+1] +  \
+                world[x][y-1] +  \
+                world[x][y+1] +  \
+                world[x+1][y-1] +  \
+                world[x+1][y] +  \
+                world[x+1][y+1]
+            if world[x][y] == 0:
+                if n == 3:
+                    newWorld[x][y] = 1
+                else:
+                    newWorld[x][y] = 0
+            
+            else:
+                if n == 3 or n ==2 :
+                    newWorld[x][y] = 1
+                else:
+                    newWorld[x][y] = 0
+    return newWorld
+    
+    
+    
 
 main()
